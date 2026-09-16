d10ec95ea65f6d129701d44629d862fabb230fbb
bendrex <bendrex.sg@gmail.com>
2026-09-17 01:46:57 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,23 +1,38 @@
-const CACHE_NAME = 'project-tracker-v1';
+const CACHE_NAME = 'project-tracker-v2';
 
 const FILES_TO_CACHE = [
-    './',
-    './index.html',
-    './manifest.json'
+  './',
+  './index.html',
+  './manifest.json',
+  './file_00000000e27881f486a296bbb46e707f.png'
 ];
 
 self.addEventListener('install', event => {
-    event.waitUntil(
-        caches.open(CACHE_NAME).then(cache => {
-            return cache.addAll(FILES_TO_CACHE);
-        })
-    );
+  self.skipWaiting();
+
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames =>
+      Promise.all(
+        cacheNames
+          .filter(name => name !== CACHE_NAME)
+          .map(name => caches.delete(name))
+      )
+    ).then(() => self.clients.claim())
+  );
 });
 
 self.addEventListener('fetch', event => {
-    event.respondWith(
-        caches.match(event.request).then(response => {
-            return response || fetch(event.request);
-        })
-    );
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
 });
